9433267c66f9c347063ca45cdc16f7a66e668f4d
Zonz-Ly <32699823+Zonz-Ly@users.noreply.github.com>
2024-11-27 12:48:26 +0800

    Update autoconfig.js

--- autoconfig.js
+++ autoconfig.js
@@ -1,3 +1,6 @@
 pref("general.config.filename", "config.js");
 pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
+
+// Remove the line below to re-enable updates.
+pref("app.update.channel", "");
