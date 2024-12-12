5fb956e13dbaf4a75216bf7f302576393beb9e18
Zonz-Ly <32699823+Zonz-Ly@users.noreply.github.com>
2024-12-12 10:13:57 +0800

    Adapt to Firefox version 133.0.3

--- autoconfig.js
+++ autoconfig.js
@@ -1,6 +1,7 @@
 pref("general.config.filename", "config.js");
 pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
+pref("svg.context-properties.content.enabled", true);
 
 // Remove the line below to re-enable updates.
 pref("app.update.channel", "");
