2791d1a4ddc8eb036dc63f0d39b5d5d177d1ad27
Zonz-Ly <32699823+Zonz-Ly@users.noreply.github.com>
2024-12-27 15:26:39 +0800

    Update autoconfig.js

--- autoconfig.js
+++ autoconfig.js
@@ -1,6 +1,7 @@
 pref("general.config.filename", "config.js");
 pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
+pref("browser.touchmode.auto", false);
 pref("svg.context-properties.content.enabled", true);
 pref("layout.css.light-dark.enabled", true);
 pref("layout.css.nesting.enabled", true);
