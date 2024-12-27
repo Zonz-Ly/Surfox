aabbdb99e27c5907f79912047020b58adfd6bd8b
Zonz-Ly <32699823+Zonz-Ly@users.noreply.github.com>
2024-12-28 02:18:19 +0800

    Update autoconfig.js

--- autoconfig.js
+++ autoconfig.js
@@ -1,10 +1,5 @@
 pref("general.config.filename", "config.js");
 pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
-pref("browser.touchmode.auto", false);
-pref("svg.context-properties.content.enabled", true);
-pref("layout.css.light-dark.enabled", true);
-pref("layout.css.nesting.enabled", true);
 
-// Remove the line below to re-enable updates.
-pref("app.update.channel", "");
+// Do not touch the three lines above.
